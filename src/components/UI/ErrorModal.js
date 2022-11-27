42b0782bb3992ef5614992850cee1b27db6e2a0b
wasim-ay <wasimbaig000@gmail.com>
2022-11-27 13:01:54 +0530

    finish build

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,24 +1,41 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import styles from "./ErrorModal.module.css";
 import Button from "./Button";
 import Card from "./Card";
 
-const ErrorModal = ({ title, message, onConfirm }) => {
+const Backdrop = ({ onConfirm }) => {
+	return <div onClick={onConfirm} className={styles.backdrop} />;
+};
+
+const ModalOverlay = ({ title, message, onConfirm }) => {
 	return (
-		<div>
-			<div onClick={onConfirm} className={styles.backdrop} />
-			<Card className={styles.modal}>
-				<header className={styles.header}>
-					<h2>{title}</h2>
-				</header>
-				<div className={styles.content}>
-					<p>{message}</p>
-				</div>
-				<footer className={styles.actions}>
-					<Button onClick={onConfirm}>Ok</Button>
-				</footer>
-			</Card>
-		</div>
+		<Card className={styles.modal}>
+			<header className={styles.header}>
+				<h2>{title}</h2>
+			</header>
+			<div className={styles.content}>
+				<p>{message}</p>
+			</div>
+			<footer className={styles.actions}>
+				<Button onClick={onConfirm}>Ok</Button>
+			</footer>
+		</Card>
+	);
+};
+
+const ErrorModal = ({ onConfirm, title, message }) => {
+	return (
+		<>
+			{ReactDOM.createPortal(
+				<Backdrop onConfirm={onConfirm} />,
+				document.getElementById("backdrop-root")
+			)}
+			{ReactDOM.createPortal(
+				<ModalOverlay title={title} message={message} onConfirm={onConfirm} />,
+				document.getElementById("overlay-root")
+			)}
+		</>
 	);
 };
 
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,25 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import Wrapper from "../Helpers/Wrapper";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import ErrorModal from "../UI/ErrorModal";
 import styles from "./AddUser.module.css";
 
 const AddUsers = ({ onAddUser }) => {
-	const [enteredUsername, setEnteredUsername] = useState("");
-	const [enteredAge, setEnteredAge] = useState("");
+	const nameInputRef = useRef();
+	const ageInputRef = useRef();
+
 	const [error, setError] = useState();
-
-	const userNameChangeHandler = (e) => {
-		setEnteredUsername(e.target.value);
-	};
-
-	const userAgeChangeHandler = (e) => {
-		setEnteredAge(e.target.value);
-	};
 
 	const addUserHandler = (e) => {
 		e.preventDefault();
-		if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+
+		const enteredName = nameInputRef.current.value;
+		const enteredAge = ageInputRef.current.value;
+
+		if (enteredName.trim().length === 0 || enteredAge.trim().length === 0) {
 			setError({
 				title: "Invalid input",
 				message: "Please enter a valid name and age (non-empty values)",
@@ -33,9 +31,9 @@ const AddUsers = ({ onAddUser }) => {
 			});
 			return;
 		}
-		onAddUser(enteredUsername, enteredAge);
-		setEnteredUsername("");
-		setEnteredAge("");
+		onAddUser(enteredName, enteredAge);
+		nameInputRef.current.value = "";
+		ageInputRef.current.value = "";
 	};
 
 	const errorHandler = () => {
@@ -43,7 +41,7 @@ const AddUsers = ({ onAddUser }) => {
 	};
 
 	return (
-		<div>
+		<Wrapper>
 			{error && (
 				<ErrorModal
 					title={error.title}
@@ -58,21 +56,19 @@ const AddUsers = ({ onAddUser }) => {
 						id="username"
 						type="text"
 						placeholder="Enter username"
-						value={enteredUsername}
-						onChange={userNameChangeHandler}
+						ref={nameInputRef}
 					/>
 					<label htmlFor="age">Age (Years)</label>
 					<input
 						id="age"
 						type="number"
 						placeholder="Enter age"
-						value={enteredAge}
-						onChange={userAgeChangeHandler}
+						ref={ageInputRef}
 					/>
 					<Button type="submit">Add User</Button>
 				</form>
 			</Card>
-		</div>
+		</Wrapper>
 	);
 };
 
